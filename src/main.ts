de7498c2cf0e88355e9e6ac95cb6f43eca556f42
D'Arcy Rittich <drittich@gmail.com>
2022-12-04 08:49:59 -0500

    add volume control

--- src/main.ts
+++ src/main.ts
@@ -28,33 +28,38 @@ let flatNotes = [
 ];
 
 let isLobbing = false;
-let pregressBar = (document.getElementById("elapsedPerThousand") as HTMLProgressElement);
-let voiceOptionsSelect = (document.getElementById('voiceOptions') as HTMLSelectElement);
-let toggleLobbingButton = (document.getElementById('toggleLobbing') as HTMLButtonElement);
-let naturalNotesCheckbox = (document.getElementById("naturalNotes") as HTMLInputElement);
-let sharpNotesCheckbox = (document.getElementById("sharpNotes") as HTMLInputElement);
-let flatNotesCheckbox = (document.getElementById("flatNotes") as HTMLInputElement);
+
+let enableSpeechCheckboxEl = $('enableSpeech') as HTMLInputElement;
+let flatNotesCheckboxEl = $("flatNotes") as HTMLInputElement;
+let keyDisplayEl = $('key') as HTMLElement;
+let keyIntervalInputEl = $('keyInterval') as HTMLInputElement;
+let keyIntervalValueEl = $('keyIntervalValue') as HTMLElement;
+let naturalNotesCheckboxEl = $("naturalNotes") as HTMLInputElement;
+let noteOrderSelectEl = $('noteOrder') as HTMLSelectElement;
+let noteOrderSpacerEl = $('noteOrderSpacer') as HTMLElement;
+let pregressBarEl = $("elapsedPerThousand") as HTMLProgressElement;
+let sharpNotesCheckboxEl = $("sharpNotes") as HTMLInputElement;
+let startNoteLabelEl = $('startNoteLabel') as HTMLElement;
+let startNoteSelectEl = $('startNote') as HTMLSelectElement;
+let toggleLobbingButtonEl = $('toggleLobbing') as HTMLButtonElement;
+let voiceOptionsSelectEl = $('voiceOptions') as HTMLSelectElement;
+let volumeInputEl = $('volume') as HTMLInputElement;
+let volumeValueEl = $('volumeValue') as HTMLElement;
 
 init();
 
 function init() {
 	loadVoices();
-	updateUiFromLocalStorage();
+	updateWithSavedSettings();
 	setupHandlers();
 }
 
-function updateUiFromLocalStorage() {
+function updateWithSavedSettings() {
 	getKeyIntervalFromLocalStorage();
 	getNoteSelectionFromLocalStorage();
 	getEnableAudioFromLocalStorage();
 	getNoteOrderFromLocalStorage();
 	populateStartNoteOptions();
-
-
-	// get voice
-	// get note selections
-	// get note order
-	// get starting note
 }
 
 function loadVoices() {
@@ -65,7 +70,7 @@ function loadVoices() {
 
 			option.value = voice.name;
 			option.innerHTML = voice.name;
-			voiceOptionsSelect.appendChild(option);
+			voiceOptionsSelectEl.appendChild(option);
 		});
 		getVoiceFromLocalStorage();
 	})
@@ -73,112 +78,113 @@ function loadVoices() {
 
 function setupHandlers() {
 	// when keyInterval changes, update the keyIntervalValue
-	document.getElementById('keyInterval')!.addEventListener('input', function () {
-		document.getElementById('keyIntervalValue')!.innerHTML = (this as HTMLInputElement).value;
+	keyIntervalInputEl!.addEventListener('input', function () {
+		keyIntervalValueEl!.innerHTML = (this as HTMLInputElement).value;
 	});
 
 	//when enableSpeech is unchecked, hide the voiceOptions
-	document.getElementById('enableSpeech')!.addEventListener('change', function () {
+	enableSpeechCheckboxEl!.addEventListener('change', function () {
 		let checked = (this as HTMLInputElement).checked;
-		voiceOptionsSelect.style.display = checked ? 'block' : 'none';
+		$('voiceOptionsContainer')!.style.display = checked ? 'block' : 'none';
 		localStorage.setItem("enableSpeech", checked.toString());
 	});
 
 	//when we change the voice, save the value to local storage
-	voiceOptionsSelect.addEventListener("change", function () {
-		var voice = voiceOptionsSelect.value;
+	voiceOptionsSelectEl.addEventListener("change", function () {
+		var voice = voiceOptionsSelectEl.value;
 		localStorage.setItem("voice", voice);
 	});
 
 	//when we change the key interval, save the value to local storage
-	document.getElementById("keyInterval")!.addEventListener("change", function () {
+	keyIntervalInputEl!.addEventListener("change", function () {
 		// get the key interval
-		var keyInterval = (document.getElementById("keyInterval") as HTMLInputElement).value;
+		var keyInterval = keyIntervalInputEl.value;
 		// save the key interval to local storage
 		localStorage.setItem("keyInterval", keyInterval);
 	});
 
 	//when we change the notes selections, save the values to local storage
-	document.getElementById("naturalNotes")!.addEventListener("change", function () {
-		let checked = (document.getElementById("naturalNotes") as HTMLInputElement).checked;
-		localStorage.setItem("naturalNotes", checked.toString());
+	naturalNotesCheckboxEl.addEventListener("change", function () {
+		localStorage.setItem("naturalNotes", naturalNotesCheckboxEl.checked.toString());
 		populateStartNoteOptions();
 	});
-	document.getElementById("flatNotes")!.addEventListener("change", function () {
-		let checked = (document.getElementById("flatNotes") as HTMLInputElement).checked;
-		localStorage.setItem("flatNotes", checked.toString());
+	flatNotesCheckboxEl.addEventListener("change", function () {
+		localStorage.setItem("flatNotes", flatNotesCheckboxEl.checked.toString());
 		populateStartNoteOptions();
 	});
-	document.getElementById("sharpNotes")!.addEventListener("change", function () {
-		let checked = (document.getElementById("sharpNotes") as HTMLInputElement).checked;
-		localStorage.setItem("sharpNotes", checked.toString());
+	sharpNotesCheckboxEl.addEventListener("change", function () {
+		localStorage.setItem("sharpNotes", sharpNotesCheckboxEl.checked.toString());
 		populateStartNoteOptions();
 	});
 
 	//when we change the note order, save the values to local storage
-	document.getElementById("noteOrder")!.addEventListener("change", function () {
-		let noteOrder = (document.getElementById("noteOrder") as HTMLInputElement).value;
+	noteOrderSelectEl!.addEventListener("change", function () {
+		let noteOrder = noteOrderSelectEl.value;
 		localStorage.setItem("noteOrder", noteOrder);
-		document.getElementById("startNoteLabel")!.style.display = noteOrder == "random" ? 'none' : 'block';
-		document.getElementById("noteOrderSpacer")!.style.display = noteOrder == "random" ? 'block' : 'none';
+		startNoteLabelEl!.style.display = noteOrder == "random" ? 'none' : 'block';
+		noteOrderSpacerEl!.style.display = noteOrder == "random" ? 'block' : 'none';
 	});
 
 	//when we change the start note, save the values to local storage
-	document.getElementById("startNote")!.addEventListener("change", function () {
-		let startNote = (document.getElementById("startNote") as HTMLInputElement).value;
+	startNoteSelectEl!.addEventListener("change", function () {
+		let startNote = startNoteSelectEl.value;
 		localStorage.setItem("startNote", startNote);
 	});
 
 	// setup click handler for start-lobbing
-	toggleLobbingButton.addEventListener("click", function () {
+	toggleLobbingButtonEl.addEventListener("click", function () {
 		if (isLobbing) {
 			stopKeyLobber();
 			isLobbing = false;
 		}
 		else {
 			// get the key interval
-			let keyInterval = (document.getElementById("keyInterval") as HTMLSelectElement).value;
+			let keyInterval = keyIntervalInputEl.value;
 			// start the key lobber
 			startKeyLobber(parseInt(keyInterval));
 			isLobbing = true;
 		}
 	});
+
+	// when volume changes, update the volumeValue
+	volumeInputEl!.addEventListener('input', function () {
+		volumeValueEl!.innerHTML = Math.round(parseFloat(volumeInputEl.value) * 100).toString();
+	});
 }
 
 function getKeyIntervalFromLocalStorage() {
-	let keyInterval = localStorage.getItem('keyInterval');
-	let rangeEl = document.getElementById('keyInterval') as HTMLInputElement;
-	if (keyInterval) {
+	let keyIntervalValue = localStorage.getItem('keyInterval');
+	if (keyIntervalValue) {
 		// set key interval
-		rangeEl.value = keyInterval;
+		keyIntervalInputEl.value = keyIntervalValue;
 	}
 	else {
 		// set default key interval
-		rangeEl.value = '15';
+		keyIntervalInputEl.value = '15';
 	}
-	document.getElementById('keyIntervalValue')!.innerHTML = rangeEl.value;
+	keyIntervalValueEl!.innerHTML = keyIntervalInputEl.value;
 }
 
 function getNoteSelectionFromLocalStorage() {
 	let checked = localStorage.getItem('naturalNotes');
 	if (checked == null)
 		checked = "false";
-	naturalNotesCheckbox.checked = checked == "true";
+	naturalNotesCheckboxEl.checked = checked == "true";
 
 	checked = localStorage.getItem('flatNotes');
 	if (checked == null)
 		checked = "false";
-	flatNotesCheckbox.checked = checked == "true";
+	flatNotesCheckboxEl.checked = checked == "true";
 
 	checked = localStorage.getItem('sharpNotes');
 	if (checked == null)
 		checked = "false";
-	sharpNotesCheckbox.checked = checked == "true";
+	sharpNotesCheckboxEl.checked = checked == "true";
 
-	if (!sharpNotesCheckbox.checked && !flatNotesCheckbox.checked && !naturalNotesCheckbox.checked) {
-		naturalNotesCheckbox.checked = true;
-		flatNotesCheckbox.checked = true;
-		sharpNotesCheckbox.checked = true;
+	if (!sharpNotesCheckboxEl.checked && !flatNotesCheckboxEl.checked && !naturalNotesCheckboxEl.checked) {
+		naturalNotesCheckboxEl.checked = true;
+		flatNotesCheckboxEl.checked = true;
+		sharpNotesCheckboxEl.checked = true;
 	}
 }
 
@@ -186,22 +192,21 @@ function getEnableAudioFromLocalStorage() {
 	let checked = localStorage.getItem('enableSpeech');
 	if (checked == null)
 		checked = "false";
-	(document.getElementById("enableSpeech") as HTMLInputElement).checked = checked == "true";
-	voiceOptionsSelect.style.display = checked == "true" ? 'block' : 'none';
+	enableSpeechCheckboxEl.checked = checked == "true";
+	voiceOptionsSelectEl.style.display = checked == "true" ? 'block' : 'none';
 }
 
 function getNoteOrderFromLocalStorage() {
 	let noteOrder = localStorage.getItem('noteOrder');
 	if (noteOrder == null)
 		noteOrder = "ascending";
-	(document.getElementById("noteOrder") as HTMLInputElement).value = noteOrder;
-	document.getElementById("startNoteLabel")!.style.display = noteOrder == "random" ? 'none' : 'block';
-	document.getElementById("noteOrderSpacer")!.style.display = noteOrder == "random" ? 'block' : 'none';
+	noteOrderSelectEl.value = noteOrder;
+	startNoteLabelEl!.style.display = noteOrder == "random" ? 'none' : 'block';
+	noteOrderSpacerEl!.style.display = noteOrder == "random" ? 'block' : 'none';
 }
 
 function getStartNoteFromLocalStorage() {
 	let startNote = localStorage.getItem('startNote');
-	let selectEl = (document.getElementById("startNote") as HTMLSelectElement);
 
 	//if startnote is not available, then set it to the first note in the list
 	let notes = getNotes();
@@ -213,18 +218,18 @@ function getStartNoteFromLocalStorage() {
 		}
 	}
 	if (found)
-		selectEl.value = startNote!;
+		startNoteSelectEl.value = startNote!;
 	else
-		selectEl.value = selectEl.getElementsByTagName('option')[0].value;
+		startNoteSelectEl.value = startNoteSelectEl.getElementsByTagName('option')[0].value;
 }
 
 function getNotes() {
 	let notes: string[][] = [];
-	if ((document.getElementById("naturalNotes") as HTMLInputElement).checked)
+	if (naturalNotesCheckboxEl.checked)
 		notes = notes.concat(naturalNotes);
-	if ((document.getElementById("flatNotes") as HTMLInputElement).checked)
+	if (flatNotesCheckboxEl.checked)
 		notes = notes.concat(flatNotes);
-	if ((document.getElementById("sharpNotes") as HTMLInputElement).checked)
+	if (sharpNotesCheckboxEl.checked)
 		notes = notes.concat(sharpNotes);
 
 	return notes;
@@ -250,14 +255,13 @@ function populateStartNoteOptions() {
 		return 0;
 	});
 
-	let startNote = document.getElementById("startNote") as HTMLInputElement;
-	startNote.innerHTML = "";
+	startNoteSelectEl.innerHTML = "";
 	notes.forEach(note => {
 		var option = document.createElement('option');
 
 		option.value = note[0];
 		option.innerHTML = note[0];
-		startNote.appendChild(option);
+		startNoteSelectEl.appendChild(option);
 	});
 	getStartNoteFromLocalStorage();
 }
@@ -265,12 +269,12 @@ function populateStartNoteOptions() {
 function getVoiceFromLocalStorage() {
 	let voice = localStorage.getItem('voice');
 	if (voice == null)
-		voiceOptionsSelect.value = voiceOptionsSelect.getElementsByTagName('option')[0].value;
+		voiceOptionsSelectEl.value = voiceOptionsSelectEl.getElementsByTagName('option')[0].value;
 	else
-		voiceOptionsSelect.value = voice;
+		voiceOptionsSelectEl.value = voice;
 }
 
-let timer:number;
+let timer: number;
 function startKeyLobber(intervalSeconds: number) {
 	getKey(intervalSeconds * 1000)
 
@@ -279,9 +283,9 @@ function startKeyLobber(intervalSeconds: number) {
 		getKey(intervalSeconds * 1000)
 	}, intervalSeconds * 1000);
 
-	toggleLobbingButton.innerHTML = "Stop Lobbing";
-	(document.getElementById("keyInterval") as HTMLInputElement).disabled = true;
-	toggleLobbingButton.classList.add("secondary");
+	toggleLobbingButtonEl.innerHTML = "Stop Lobbing";
+	keyIntervalInputEl.disabled = true;
+	toggleLobbingButtonEl.classList.add("secondary");
 }
 
 function stopKeyLobber() {
@@ -289,9 +293,9 @@ function stopKeyLobber() {
 	clearInterval(timer);
 	clearInterval(bgTimer);
 
-	toggleLobbingButton.innerHTML = "Start Lobbing";
-	(document.getElementById("keyInterval") as HTMLSelectElement).disabled = false;
-	toggleLobbingButton.classList.remove("secondary");
+	toggleLobbingButtonEl.innerHTML = "Start Lobbing";
+	keyIntervalInputEl.disabled = false;
+	toggleLobbingButtonEl.classList.remove("secondary");
 }
 
 let bgTimer: number;
@@ -299,18 +303,19 @@ let elapsedMs = 0;
 function getKey(intervalMs: number) {
 	clearInterval(bgTimer);
 	let key = getRandomKey();
-	document.getElementById("key")!.innerHTML = key[0];
+	keyDisplayEl.innerHTML = key[0];
 	elapsedMs = 0;
 
-	if ((document.getElementById("enableSpeech") as HTMLInputElement).checked) {
+	if (enableSpeechCheckboxEl.checked) {
 		let utterance = new SpeechSynthesisUtterance(key[1]);
 		utterance.pitch = 1;
 		utterance.rate = .9;
+		utterance.volume = parseFloat(volumeInputEl.value);
 
 		// If a voice has been selected, find the voice and set the
 		// utterance instance's voice attribute.
-		if (voiceOptionsSelect.value) 
-			utterance.voice = speechSynthesis.getVoices().filter(function (voice) { return voice.name == voiceOptionsSelect.value; })[0];
+		if (voiceOptionsSelectEl.value)
+			utterance.voice = speechSynthesis.getVoices().filter(function (voice) { return voice.name == voiceOptionsSelectEl.value; })[0];
 
 		speechSynthesis.speak(utterance);
 	}
@@ -323,7 +328,7 @@ function getKey(intervalMs: number) {
 			clearInterval(bgTimer);
 		} else {
 			elapsedMs += frameMs;
-			pregressBar.value = Math.round((elapsedMs / intervalMs) * 1000);
+			pregressBarEl.value = Math.round((elapsedMs / intervalMs) * 1000);
 		}
 	}
 }
@@ -339,4 +344,8 @@ function getRandomKey() {
 	} while (nextKey[0] == currentKey[0]);
 	currentKey = nextKey;
 	return currentKey;
+}
+
+function $(id: string) {
+	return document.getElementById(id);
 }
